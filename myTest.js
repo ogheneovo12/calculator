10c4e90ed0a795502526998797ae9acb15166399
ogheneovo12 <ukuanovweogheneovo@gmail.com>
2020-07-26 03:27:16 +0100

    added font cdn

--- myTest.js
+++ myTest.js
@@ -11,7 +11,7 @@ let firstNumber = 0,
   operation = "";
 const operatorsList = ["+", "*", "/", "%", "-", "^", "=", "c", "root"];
 const getConstraint = () => output.value == result;
-const isTerminalOperator = value => value =="c" || value == "="
+const isTerminalOperator = (value) => value == "c" || value == "=";
 function addListEventListener(elements, handler = () => {}, event = "click") {
   if (!elements) {
     throw Error("no element was added");
@@ -23,11 +23,15 @@ function addListEventListener(elements, handler = () => {}, event = "click") {
 function updateTopOutput(value) {
   topOutput.value += output.value + value;
 }
-const isSingleOperator =()=> (/^([\w\d]+?)([+\*/-]{1})$/g).test(topOutput.value)
+const isSingleOperator = () =>
+  /^([\w\d]+?)([+\*/-]{1})$/g.test(topOutput.value);
 
 function checkIf_changeOperation(newOperator) {
   const lastOperation = topOutput.value[topOutput.value.length - 1];
-  if (operatorsList.includes(lastOperation) && lastOperation !== newOperator || isSingleOperator()) {
+  if (
+    (operatorsList.includes(lastOperation) && lastOperation !== newOperator) ||
+    isSingleOperator()
+  ) {
     operation = newOperator;
     topOutput.value =
       topOutput.value.slice(0, topOutput.value.length - 1) + operation;
@@ -46,7 +50,7 @@ function checkIf_startAgain(value) {
 }
 
 //Event handler
-const handleButtonClick = e => {
+const handleButtonClick = (e) => {
   e.preventDefault();
   checkIf_startAgain(e.target.value);
   if (output.value == "0" || operatorIspressed) {
@@ -57,45 +61,47 @@ const handleButtonClick = e => {
   operatorIspressed = false;
 };
 
-const handleOperatorClick =  e => {
+const handleOperatorClick = (e) => {
   e.preventDefault();
   //just house keeping check
   if (!operatorsList.includes(e.target.value)) {
     return;
   }
   //sets iSanswered status to false if an operator is clicked, after a calculation session has ended
-  if(isAnswered){
-    isAnswered = false
+  if (isAnswered) {
+    isAnswered = false;
   }
-  
-  //allows change operator on a single operator 
-  if(operatorIspressed && isSingleOperator() && !isTerminalOperator(e.target.value)){
-    checkIf_changeOperation(e.target.value)
-    return
-}
+
+  //allows change operator on a single operator
+  if (
+    operatorIspressed &&
+    isSingleOperator() &&
+    !isTerminalOperator(e.target.value)
+  ) {
+    checkIf_changeOperation(e.target.value);
+    return;
+  }
   //signals that an operator has been pressed
   operatorIspressed = true;
-/**
+  /**
    * prevents operator from working on result multiple times, when clicked intermitently
    * meaning a single operator shouldn't be clicked more than once
    *  and to avoid extra logical check
-  */
+   */
   if (getConstraint() && !isTerminalOperator(e.target.value)) {
     //if a new operator is clicked then change previous operation
     checkIf_changeOperation(e.target.value);
     return;
   }
 
+  //print to top
+  updateTopOutput(e.target.value);
 
-//print to top
-updateTopOutput(e.target.value);
-
- if (e.target.value == "=") {
+  if (e.target.value == "=") {
     calculate();
     isAnswered = true;
     return;
-  }
-  else if (e.target.value == "c") {
+  } else if (e.target.value == "c") {
     clear();
     return;
   }
@@ -103,15 +109,13 @@ updateTopOutput(e.target.value);
     calculate();
     return;
   }
-firstNumber = parseInt(output.value);
- if(e.target.value == "^"){
-   operation = "**"
-   return
- }
+  firstNumber = parseInt(output.value);
+  if (e.target.value == "^") {
+    operation = "**";
+    return;
+  }
   operation = e.target.value;
-  
 };
-
 
 function calculate() {
   //dont do anything if no operation was selected
@@ -130,12 +134,12 @@ function calculate() {
  * Function is meant to clear memory and start calculator from fres
  */
 function clear() {
-     topOutput.value = "";
-     output.value = "0";
-     result = 0;
-     firstNumber = 0;
-     secondNumber = 0;
-     operation = ""
+  topOutput.value = "";
+  output.value = "0";
+  result = 0;
+  firstNumber = 0;
+  secondNumber = 0;
+  operation = "";
 }
 
 //addEventListener
